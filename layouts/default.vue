9097e56394ab34f6be061156c6095461d1d150cf
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-07-05 23:35:06 +0100

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>This is the header</div>
+    <nuxt />
+    <div>This is the footer</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "default"
+}
+</script>
+
+<style scoped>
+
+</style>
+
