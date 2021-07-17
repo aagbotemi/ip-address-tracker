d0e826906212a49aab097e75cd6a0d62b55bd783
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-07-18 00:01:17 +0100

    ip tracker map set to component

---
+++ components/IPTrackerMap.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="map-wrap" style="height: 100vh">
+    <client-only>
+      <l-map :zoom='zoom' :center="[latitude, longitude]">
+        <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+        <l-marker :lat-lng="[latitude, longitude]"></l-marker>
+      </l-map>
+    </client-only>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IPTrackerMap",
+  props: {
+    latitude: {
+      type: null,
+      required: true
+    },
+    longitude: {
+      type: null,
+      required: true
+    },
+    zoom: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -17,23 +17,18 @@
       </form>
       <IPTrackerCard :ip-address="ipAddress"/>
     </header>
-    <div id="map-wrap" style="height: 100vh">
-      <client-only>
-        <l-map :zoom='zoom' :center="[latitude, longitude]">
-          <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
-          <l-marker :lat-lng="[latitude, longitude]"></l-marker>
-        </l-map>
-      </client-only>
-    </div>
+    <IPTrackerMap :zoom="zoom" :latitude="latitude" :longitude="longitude" />
   </div>
 </template>
 
 <script>
 import 'leaflet/dist/leaflet.css'
 import IPTrackerCard from "../components/IPTrackerCard";
+import IPTrackerMap from "../components/IPTrackerMap";
 
 export default {
   components: {
+    IPTrackerMap,
     IPTrackerCard,
   },
   data: () => ({
