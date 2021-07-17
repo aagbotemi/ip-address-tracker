81f65d61f9be84a4d7a5b3b0266189e10ae0127f
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-07-17 20:58:29 +0100

    designed the ui

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <div>This is the header</div>
+<!--    <div>This is the header</div>-->
     <nuxt />
-    <div>This is the footer</div>
+<!--    <div>This is the footer</div>-->
   </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,41 @@
 <template>
   <div v-if="loading">Loading...</div>
   <div v-else>
-    <header >
-      <h1>IP Address Tracker</h1>
-      {{ latitude }}
-      {{ longitude }}
-      <form @submit.prevent="getIP">
+    <header class="text-center py-7">
+      <h1 class="text-white text-2xl sm:text-4xl">IP Address Tracker</h1>
+      <form class="flex justify-center align-center mt-5 mb-7 sm:mb-10" @submit.prevent="getIP">
         <input
+          class="shadow border rounded-l w-80 text-gray-700 sm:py-3 py-2 px-3 focus:outline-none focus:shadow-outline"
           v-model="search"
           type="text"
           name="search"
           placeholder="Search for any IP address or domain"
         />
-        <button type="submit"><img src="/images/icon-arrow.svg"></button>
+        <button class="bg-white hover:bg-purple-600 rounded-r px-3" type="submit">
+          <img src="/images/icon-arrow.svg">
+        </button>
       </form>
-
-      <section class="details">
-        <article>
-          <div class="ip-address">
-            <h3>IP ADDRESS</h3>
-            <span>{{ ipAddress.ip }}</span>
+      <section class=" w-full flex justify-center absolute">
+        <article class="sm:flex bg-white shadow-md rounded-md py-5 px-16 sm:p-5 mx-5">
+          <div class="border-b sm:border-b-0 sm:border-r border-grey-900 sm:pr-3 pb-2 sm:pb-0">
+            <h3 class="text-xs font-bold text-gray-500">IP ADDRESS</h3>
+            <span class="font-medium text-lg sm:text-xl">{{ ipAddress.ip }}</span>
           </div>
-          <div class="location">
-            <h3>LOCATION</h3>
-            <span>
+          <div class="border-b sm:border-b-0 sm:border-r border-grey-900 sm:px-3 py-2 sm:py-0">
+            <h3 class="text-xs font-bold text-gray-500">LOCATION</h3>
+            <span class="font-medium text-lg sm:text-xl">
               {{ ipAddress.location && ipAddress.location.city }},
               {{ ipAddress.location && ipAddress.location.region }},
               {{ ipAddress.location && ipAddress.location.country }}
             </span>
           </div>
-          <div class="timezone">
-            <h3>TIMEZONE</h3>
-            <span>GMT {{ ipAddress.location && ipAddress.location.timezone }}</span>
+          <div class="border-b sm:border-b-0 sm:border-r border-grey-900 sm:px-3 py-2 sm:py-0">
+            <h3 class="text-xs font-bold text-gray-500">TIMEZONE</h3>
+            <span class="font-medium text-lg sm:text-xl">GMT {{ ipAddress.location && ipAddress.location.timezone }}</span>
           </div>
-          <div class="isp">
-            <h3>ISP</h3>
-            <span>{{ ipAddress.isp }}</span>
+          <div class=" sm:pl-3 pt-2 sm:pt-0">
+            <h3 class="text-xs font-bold text-gray-500">ISP</h3>
+            <span class="font-medium text-lg sm:text-xl">{{ ipAddress.isp }}</span>
           </div>
         </article>
       </section>
@@ -47,12 +47,6 @@
 // import axios from '@nuxtjs/axios'
 
 export default {
-  /* async asyncData({ $axios, $config }) {
-    const post = await $axios.$get($config.baseUrl + '?apiKey=' + $config.apiKey + `&ipAddress=${search}`);
-
-    console.log(post)
-    return { post }
-  }, */
   data: () => ({
     loading: false,
     search: '',
@@ -69,7 +63,6 @@ export default {
       try {
         await this.$axios.get(this.$config.baseUrl + '?apiKey=' + this.$config.apiKey + `&ipAddress=${this.search}`)
           .then(res => {
-            console.log(res.data)
             this.ipAddress = res.data
             this.latitude = this.ipAddress.location.lat
             this.longitude = this.ipAddress.location.lng
@@ -80,10 +73,20 @@ export default {
       } finally {
         this.loading = false
       }
-
-
-      // this.loading = false
     }
   },
 }
 </script>
+
+<style >
+*, ::before, ::after {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+header {
+  background-image: url('/images/pattern-bg.png');
+  min-height: 15rem;
+}
+</style>
